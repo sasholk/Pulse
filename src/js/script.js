9a8a5c51c1981525ab607d1f2e2e80f6bc3c911b
sasholk <iccc97@gmail.com>
2021-08-11 17:09:53 +0300

    validate index.html

--- src/js/script.js
+++ src/js/script.js
@@ -99,4 +99,16 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    new WOW().init();
 });
